719c34044a4645a21ebf60e1e943ce637d15f5a4
vasja-batryn <vasjabat@icloud.com>
2022-11-18 08:38:23 +0200

    sddfsd

--- student-service/student-service-app/src/main/java/com/demo/student/utils/HelperUtil.java
+++ student-service/student-service-app/src/main/java/com/demo/student/utils/HelperUtil.java
@@ -21,7 +21,7 @@ public class HelperUtil {
             Arrays.asList(
                     Student.builder().name("Student1").build(),
                     Student.builder().name("Student2").build(),
-                    Student.builder().name("Student3").build()
+                    Student.builder().name("Student3").build(),
                     Student.builder().name("Student4").build()
             );
 }
